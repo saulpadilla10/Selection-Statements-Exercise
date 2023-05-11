e774171cb07bfd6d940794160c44ad42b6b30344
saulpadilla10 <saulp1021@yahoo.com>
2023-05-11 16:08:04 -0700

    No Winning

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,37 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("Play a game? Guess my favorite number!");
+            Console.WriteLine("Input now.");
+
+            string input = Console.ReadLine();
+
+            int guessE = int.Parse(input);
+
+            var favNumber = 70;
+           
+
+            if (guessE < favNumber)
+            {
+                Console.WriteLine("Too low.");
+            }
+            else if (guessE > favNumber)
+            {
+                Console.WriteLine("Too high! Try again");
+            }
+            else
+            {
+                Console.WriteLine($"Nevermind.");
+            }
+
+
+
+
+
+
+
+
         }
     }
 }
